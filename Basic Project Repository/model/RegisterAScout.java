1b2cdfb8a242bfc917b17cd31b489d272fe7265c
cenge1 <engertcam@gmail.com>
2025-04-16 19:58:10 -0400

    scout

---
+++ Basic Project Repository/model/RegisterAScout.java
@@ -0,0 +1,29 @@
+package model;
+
+import impresario.IView;
+
+public class RegisterAScout extends EntityBase implements IView {
+
+    Scout ScoutToRegister;
+    RegisterAScout() {
+
+    }
+    public String getState(String x) {
+
+    }
+    public void stateChangeRequest(String x, Object y) {
+
+    }
+    public void initializeSchema(String x) {
+
+    }
+    public void updateState(String x, Object y) {
+
+    }
+
+
+
+
+
+
+}
